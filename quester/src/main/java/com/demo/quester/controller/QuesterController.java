11c5f0977274958de020c4034ec8b912bc338eb0
Gyanendra Ranjan <shanky.gyan@gmail.com>
2022-01-05 12:18:29 +0530

    Default in memory

--- quester/src/main/java/com/demo/quester/controller/QuesterController.java
+++ quester/src/main/java/com/demo/quester/controller/QuesterController.java
@@ -20,27 +20,28 @@ public class QuesterController {
 
 	@Autowired
 	QuesterService questerService;
-	
+
 	ObjectMapper objectMapper;
+	List<ItemDetails> foo;
+	List<ItemDetails> bar;
+	List<ItemDetails> grains;
 
 	@GetMapping("/quester/{price}")
 	@ResponseBody
 	public String getItemList(@PathVariable int price) {
 		System.out.println(price);
-		List<ItemDetails> foo = questerService.someRestCall("fruits");
-		List<ItemDetails> bar = questerService.someRestCall("vegetables");
-		List<ItemDetails> grains = questerService.someRestCall("grains");
-			 
-		 List<ItemDetails> combinedList = Stream.of(foo, bar, grains)
-                 .flatMap(x -> x.stream())
-                 .filter(x -> x.getQty() <= price)
-                 .collect(Collectors.toList());
-		 
-		if(combinedList.isEmpty())
-			return "NO_DATA_FOUND";
-		
-		objectMapper=new ObjectMapper();
+		foo = foo == null ? questerService.someRestCall("fruits") : foo;
+		bar = bar == null ? questerService.someRestCall("vegetables") : bar;
+		grains = grains == null ? questerService.someRestCall("grains") : grains;
+
+		List<ItemDetails> combinedList = Stream.of(foo, bar, grains).flatMap(x -> x.stream())
+				.filter(x -> x.getQty() <= price).collect(Collectors.toList());
+
+		if (combinedList.isEmpty())
+			return "NO_FOUND";
+
 		try {
+			objectMapper = new ObjectMapper();
 			return objectMapper.writeValueAsString(combinedList);
 		} catch (JsonProcessingException e) {
 			// TODO Auto-generated catch block
